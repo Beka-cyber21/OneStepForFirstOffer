3565d7117c61fa1134e1cf5d57b312eb05c14ab2
Beka-cyber21 <bekturakmatov21@gmail.com>
2025-07-20 17:46:58 +0600

    Update ChromeWebDriver.java

--- src/main/java/finalProject/driver/ChromeWebDriver.java
+++ src/main/java/finalProject/driver/ChromeWebDriver.java
@@ -11,21 +11,21 @@ public class ChromeWebDriver {
     public static WebDriver loadChromeDriver() {
         WebDriverManager.chromedriver().setup();
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--window-size-1920,1080");
-        options.addArguments("--no-sandbox");
-        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        if (Boolean.parseBoolean(getProperty("headless"))) {
-            options.addArguments("--headless");
-        }
+        // ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--remote-allow-origins=*");
+        // options.addArguments("--disable-extensions");
+        // options.addArguments("--window-size-1920,1080");
+        // options.addArguments("--no-sandbox");
+        // options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        // if (Boolean.parseBoolean(getProperty("headless"))) {
+        //     options.addArguments("--headless");
+        // }
 
-        WebDriver driver = new ChromeDriver(options);
+        // WebDriver driver = new ChromeDriver(options);
 
-//        WebDriver driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+       WebDriver driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return driver;
 
     }
