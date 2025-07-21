e62103e17c00157618981d4282c3235a97e10a9f
Bektur <Akmatovmak99Gmail.com>
2025-07-21 11:32:46 +0600

    Update ChromeWebDriverMethods

--- src/main/java/finalProject/driver/ChromeWebDriver.java
+++ src/main/java/finalProject/driver/ChromeWebDriver.java
@@ -1,5 +1,6 @@
 package finalProject.driver;
 
+import finalProject.utilFiles.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
@@ -11,26 +12,43 @@ import java.time.Duration;
 import static finalProject.utilFiles.ConfigReader.getProperty;
 
 public class ChromeWebDriver {
+//    public static WebDriver loadChromeDriver() {
+//        WebDriverManager.chromedriver().setup();
+
+    private static WebDriver driver;
+
     public static WebDriver loadChromeDriver() {
-        WebDriverManager.chromedriver().setup();
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
 
-        // ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--remote-allow-origins=*");
-        // options.addArguments("--disable-extensions");
-        // options.addArguments("--window-size-1920,1080");
-        // options.addArguments("--no-sandbox");
-        // options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        // if (Boolean.parseBoolean(getProperty("headless"))) {
-        //     options.addArguments("--headless");
-        // }
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-gpu"); // Важно для headless
+            options.addArguments("--window-size=1920,1080"); // Обязательно для headless
 
-        // WebDriver driver = new ChromeDriver(options);
+            // Новый headless режим для Chrome >=109
+            if (Boolean.parseBoolean(ConfigReader.getProperty("headless"))) {
+                options.addArguments("--headless=new");
+            }
 
-       WebDriver driver = new ChromeDriver();
-       driver.manage().window().maximize();
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
         return driver;
-
     }
 
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
+
+
+
